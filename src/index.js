7a40df2a1c13f5118efee428ba1ec26cb087e88c
Shiv Anand <gangwar.shiv@gmail.com>
2021-04-24 21:48:17 +0530

    Store is added to the app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
+import "./index.css";
+import App from "./components/App";
+import movies from "./reducers";
 
-ReactDOM.render(
-  <React>
-    <App />
-  </React>,
-  document.getElementById('root')
-);
+//store need reducer as an argument
+const store = createStore(movies);
+console.log("store", store);
+console.log("Before State", store.getState());
 
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "superman" }],
+});
+console.log("After State", store.getState());
 
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 export default function movies(state=[],action){
-    if(action.type=='ADD_MOVIES'){
+    if(action.type==='ADD_MOVIES'){
         return action.movies;
     }
     return state;
