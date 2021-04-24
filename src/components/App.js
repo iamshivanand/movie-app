19413a5f1f3804088aec1561505ade131276e6af
Shiv Anand <gangwar.shiv@gmail.com>
2021-04-24 22:32:34 +0530

    intital state is change to have a object with properties list and favourites

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   }
 
   render() {
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState();
     return (
       <div className="App">
         <Navbar />
@@ -33,7 +33,7 @@ class App extends React.Component {
           </div>
 
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
 import { ADD_MOVIES } from "../actions";
 
+const initialMoviesState={
+    list:[],
+    favourites:[]
+}
 
-export default function movies(state = [], action) {
+export default function movies(state = initialMoviesState, action) {
   if (action.type === ADD_MOVIES) {
-    return action.movies;
+    return {
+        ...state,
+        list:action.movies
+    }
   }
   return state;
 }
